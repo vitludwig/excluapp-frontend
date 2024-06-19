a87a886f9c5f21fcbd9074c6a61043581aa22796
Vít Ludwig <vit.ludwig1@gmail.com>
2024-06-19 23:07:36 +0200

    added non empty kegs check to payday

--- src/app/modules/admin/components/payments/components/payday/payday.component.ts
+++ src/app/modules/admin/components/payments/components/payday/payday.component.ts
@@ -2,9 +2,12 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { TableModule } from 'primeng/table';
 import { ToggleButtonModule } from 'primeng/togglebutton';
@@ -20,7 +23,21 @@ import { PaydayTableComponent } from '../payday-table/payday-table.component';
 @Component({
 	selector: 'app-payday',
 	standalone: true,
-	imports: [CommonModule, MultiSelectModule, FormsModule, DropdownModule, ButtonModule, TableModule, SortPipe, PaydayTableComponent, ToggleButtonModule, InputSwitchModule],
+	imports: [
+		CommonModule,
+		MultiSelectModule,
+		FormsModule,
+		DropdownModule,
+		ButtonModule,
+		TableModule,
+		SortPipe,
+		PaydayTableComponent,
+		ToggleButtonModule,
+		InputSwitchModule,
+		MessageModule,
+		InputTextModule,
+		RouterLink,
+	],
 	templateUrl: './payday.component.html',
 	styleUrls: ['./payday.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -42,12 +59,26 @@ export class PaydayComponent {
 
 		return events;
 	});
+	protected $eventsWithFullKegs = computed(() => {
+		const selectedEvents = this.$selectedEvents();
+		return selectedEvents.filter((event) => {
+			const eventKegs = this.$copySortiment().filter((keg) => event.kegs.includes(keg.id));
+			return eventKegs.some((keg) => !keg.isEmpty);
+		});
+	});
 
 	protected $showOnlyNotPaidEvents = signal<boolean>(true);
 	private $copySortiment = signal<IKeg[]>([]);
 
 	constructor() {
-		// TODO: get events to make payday of from server
+		this.loadSortiment();
+	}
+
+	/**
+	 * We load sortiment, so we can filter out events with paid/unpaid kegs
+	 * TODO: get events to make payday of from server
+	 */
+	private loadSortiment() {
 		this.sortimentService
 			.getSortimentList(undefined, { isOriginal: false })
 			.pipe(takeUntilDestroyed())
@@ -78,5 +109,23 @@ export class PaydayComponent {
 		);
 
 		this.$paydayResult.set(result);
+	}
+
+	/**
+	 * Refresh local array of all copy sortiment with new values from server
+	 * for example if user sets keg as empty
+	 * @protected
+	 */
+	protected refreshEventsWithFullKegs() {
+		const eventKegs = this.$selectedEvents().flatMap((event) => event.kegs);
+
+		this.sortimentService
+			.getSortimentList(eventKegs)
+			.pipe(map((kegs) => kegs.reduce((obj: { [index: number]: IKeg }, item) => ((obj[item.id] = item), obj), {})))
+			.subscribe((newKegs) => {
+				this.$copySortiment.update((sortiment) => {
+					return sortiment.map((s) => newKegs[s.id] ?? s);
+				});
+			});
 	}
 }
