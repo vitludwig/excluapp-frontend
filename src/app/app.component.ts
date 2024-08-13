5e6531e58a283432460408b99c779b6b77b13088
Vít Ludwig <vit.ludwig1@gmail.com>
2024-08-13 13:38:32 +0200

    added network connection check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { ConfirmationService } from 'primeng/api';
 import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { ToastModule } from 'primeng/toast';
-import { filter, tap } from 'rxjs';
+import { filter, fromEvent, map, merge, tap } from 'rxjs';
+import { NotificationService } from './common/services/notification.service';
 import { SidebarComponent } from './layout/components/sidebar/sidebar.component';
 import { TopbarComponent } from './layout/components/topbar/topbar.component';
 import { LayoutService } from './layout/services/layout/layout.service';
@@ -20,14 +22,41 @@ import { LayoutService } from './layout/services/layout/layout.service';
 })
 export class AppComponent {
 	private readonly router = inject(Router);
+	private readonly notificationService = inject(NotificationService);
 	protected readonly layoutService = inject(LayoutService);
+
+	protected $isOnline = signal<boolean>(navigator.onLine);
+	protected $showNetworkAlert = signal<boolean>(false);
 
 	constructor() {
 		this.router.events
 			.pipe(
+				takeUntilDestroyed(),
 				filter((e) => e instanceof NavigationEnd),
 				tap(() => this.layoutService.$sidebarVisible.set(false)),
 			)
 			.subscribe();
+
+		this.initNetworkStatusCheck();
+	}
+
+	protected initNetworkStatusCheck(): void {
+		merge(fromEvent(window, 'online'), fromEvent(window, 'offline'))
+			.pipe(
+				takeUntilDestroyed(),
+				map(() => navigator.onLine),
+			)
+			.subscribe((newStatus) => {
+				this.$showNetworkAlert.set(false);
+
+				if (!this.$isOnline() && newStatus) {
+					this.notificationService.success('Připojení k internetu je zpět!');
+				}
+				if (this.$isOnline() && !newStatus) {
+					this.$showNetworkAlert.set(true);
+				}
+
+				this.$isOnline.set(newStatus);
+			});
 	}
 }
