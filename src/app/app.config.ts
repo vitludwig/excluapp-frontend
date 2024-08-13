f5f06ada07d132340ba2df43a3a72a292f12976b
Vít Ludwig <vit.ludwig1@gmail.com>
2024-08-13 17:17:34 +0200

    angular update to v18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,25 +1,25 @@
-import { ApplicationConfig, ErrorHandler, importProvidersFrom, isDevMode } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 import { MessageService } from 'primeng/api';
 import { routes } from './app.routes';
 import { HttpErrorHandler } from './common/errors/HttpErrorHandler';
 import { InitializerService } from './common/services/initializer.service';
-import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-    provideRouter(routes),
-    provideAnimations(),
-    importProvidersFrom(HttpClientModule),
-    { provide: ErrorHandler, useClass: HttpErrorHandler },
-    MessageService,
-    InitializerService.APP_INITIALIZER_PROVIDER,
-    provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    })
-],
+		provideRouter(routes),
+		provideAnimations(),
+		provideHttpClient(),
+		{ provide: ErrorHandler, useClass: HttpErrorHandler },
+		MessageService,
+		InitializerService.APP_INITIALIZER_PROVIDER,
+		provideServiceWorker('ngsw-worker.js', {
+			enabled: !isDevMode(),
+			registrationStrategy: 'registerWhenStable:30000',
+		}),
+	],
 };
